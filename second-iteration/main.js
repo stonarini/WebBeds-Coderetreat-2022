666bdc35d354ede11007ea0719b1f645b8042fd6
stonarini <stonarini@cifpfbmoll.eu>
2022-11-06 14:31:03 +0100

    make second iteration work

--- second-iteration/main.js
+++ second-iteration/main.js
@@ -1,30 +1,32 @@
 
 
 function init(col, row, cells) {
-    let grid = [
+    let grid = []
 
-    ]
-    return [
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0],
-    ]
+    for (i of [...Array(col).keys()]) {
+        grid.push([...Array(row).fill(0)])
+    }
+
+    for (cell of cells) {
+        grid[cell[0] - 1][cell[1] - 1] = 1
+    }
+
+    return grid
 }
 
 function checkRow(grid, cell) {
-    let row = grid[cell[0] - 1]
+    let row = grid[cell[0]]
     let counter = 0
-    for (let i = cell[1] - 1; i < cell[1] + 1; i++) {
-        counter += row[i];
+    for (let i = cell[1] - 1; i <= cell[1] + 1; i++) {
+        counter += i >= 0 && i < grid[0].length ? row[i] : 0;
     }
     return counter;
 }
 
 function checkColumns(g, cell) {
     let counter = 0
-    for (let i = cell[0] - 2; i < cell[0] + 1; i++) {
-        counter += checkRow(g, [i, cell[1]])
+    for (let i = cell[0] - 1; i <= cell[0] + 1; i++) {
+        counter += i >= 0 && i < g.length ? checkRow(g, [i, cell[1]]) : 0
     }
     return counter - g[cell[0]][cell[1]]
 }
@@ -33,9 +35,11 @@ function nextTick(g) {
     let new_g = g
     for (let i = 0; i < g.length; i++) {
         for (let j = 0; j < g[i].length; j++) {
-            new_g[i][j] = checkColumns(g, [i, j]) < 2 || checkColumns(g, [i, j]) > 3 ? 0 : 1
+            let neighbours = checkColumns(g, [i, j])
+            new_g[i][j] = neighbours < 2 ? 0 : (neighbours == 3 && g[i][j] == 0) || (neighbours < 3 && g[i][j] == 1) ? 1 : 0
         }
     }
+    return new_g
 }
 
 module.exports = { init, checkRow, checkColumns, nextTick }
--- second-iteration/test.js
+++ second-iteration/test.js
@@ -2,7 +2,7 @@ const main = require("./main");
 
 
 test('initialize grid', () => {
-    expect(main.init(4, 8, [(3, 4), (3, 5), (4, 4)])).toStrictEqual(
+    expect(main.init(4, 8, [[3, 4], [3, 5], [4, 4]])).toStrictEqual(
         [
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,19 +14,19 @@ test('initialize grid', () => {
 
 test('check row', () => {
     let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
-    expect(main.checkRow(grid, [2, 4])).toBe(0);
+    expect(main.checkRow(grid, [1, 3])).toBe(0);
 });
 
 
 test('check columns', () => {
     let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
-    expect(main.checkColumns(grid, [3, 4])).toBe(2);
+    expect(main.checkColumns(grid, [2, 3])).toBe(2);
 });
 
 
 test('next tick', () => {
     let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
-    expect(main.nextTick(grid)).toBe(
+    expect(main.nextTick(grid)).toStrictEqual(
         [
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
