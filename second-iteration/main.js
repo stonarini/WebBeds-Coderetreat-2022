b603b12c1fd806e11cf1b36b397105e7f90bf73b
stonarini <stonarini@cifpfbmoll.eu>
2022-11-06 15:15:48 +0100

    use module pattern and create a public interface

--- second-iteration/main.js
+++ second-iteration/main.js
@@ -1,45 +1,56 @@
+function gameOfLife() {
 
+    var grid
 
-function init(col, row, cells) {
-    let grid = []
+    function init(col, row, cells) {
+        grid = []
 
-    for (i of [...Array(col).keys()]) {
-        grid.push([...Array(row).fill(0)])
-    }
+        for (i of [...Array(col).keys()]) {
+            grid.push([...Array(row).fill(0)])
+        }
 
-    for (cell of cells) {
-        grid[cell[0] - 1][cell[1] - 1] = 1
-    }
-
-    return grid
-}
-
-function checkRow(grid, cell) {
-    let row = grid[cell[0]]
-    let counter = 0
-    for (let i = cell[1] - 1; i <= cell[1] + 1; i++) {
-        counter += i >= 0 && i < grid[0].length ? row[i] : 0;
-    }
-    return counter;
-}
-
-function checkColumns(g, cell) {
-    let counter = 0
-    for (let i = cell[0] - 1; i <= cell[0] + 1; i++) {
-        counter += i >= 0 && i < g.length ? checkRow(g, [i, cell[1]]) : 0
-    }
-    return counter - g[cell[0]][cell[1]]
-}
-
-function nextTick(g) {
-    let new_g = g
-    for (let i = 0; i < g.length; i++) {
-        for (let j = 0; j < g[i].length; j++) {
-            let neighbours = checkColumns(g, [i, j])
-            new_g[i][j] = neighbours < 2 ? 0 : (neighbours == 3 && g[i][j] == 0) || (neighbours < 3 && g[i][j] == 1) ? 1 : 0
+        for (cell of cells) {
+            grid[cell[0] - 1][cell[1] - 1] = 1
         }
     }
-    return new_g
-}
 
-module.exports = { init, checkRow, checkColumns, nextTick }
+    function checkRow(cell) {
+        let row = grid[cell[0]]
+        let counter = 0
+        for (let i = cell[1] - 1; i <= cell[1] + 1; i++) {
+            counter += i >= 0 && i < grid[0].length ? row[i] : 0;
+        }
+        return counter;
+    }
+
+    function checkColumns(cell) {
+        let counter = 0
+        for (let i = cell[0] - 1; i <= cell[0] + 1; i++) {
+            counter += i >= 0 && i < grid.length ? checkRow([i, cell[1]]) : 0
+        }
+        return counter - grid[cell[0]][cell[1]]
+    }
+
+    function nextTick() {
+        let new_g = grid
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                let neighbours = checkColumns([i, j])
+                new_g[i][j] = neighbours < 2 ? 0 : (neighbours == 3 && grid[i][j] == 0) || (neighbours <= 3 && grid[i][j] == 1) ? 1 : 0
+            }
+        }
+        grid = new_g
+        return grid
+    }
+
+    function getBoard() {
+        return grid
+    }
+
+    return {
+        createBoard: init,
+        nextTick,
+        getBoard,
+    }
+}
+module.exports = gameOfLife()
--- second-iteration/test.js
+++ second-iteration/test.js
@@ -1,37 +1,61 @@
-const main = require("./main");
+const gameOfLife = require("./main");
 
 
-test('initialize grid', () => {
-    expect(main.init(4, 8, [[3, 4], [3, 5], [4, 4]])).toStrictEqual(
+// test('initialize grid', () => {
+//     expect(main.init(4, 8, [[3, 4], [3, 5], [4, 4]])).toStrictEqual(
+//         [
+//             [0, 0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 1, 1, 0, 0, 0],
+//             [0, 0, 0, 1, 0, 0, 0, 0],
+//         ]
+//     );
+// });
+
+// test('check row', () => {
+//     let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
+//     expect(main.checkRow(grid, [1, 3])).toBe(0);
+// });
+
+
+// test('check columns', () => {
+//     let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
+//     expect(main.checkColumns(grid, [2, 3])).toBe(2);
+// });
+
+
+// test('next tick', () => {
+//     let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
+//     expect(main.nextTick(grid)).toStrictEqual(
+//         [
+//             [0, 0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 1, 1, 0, 0, 0],
+//             [0, 0, 0, 1, 1, 0, 0, 0],
+//         ]
+//     );
+// });
+
+test("init board", () => {
+    gameOfLife.createBoard(4, 8, [[2, 5], [3, 4], [3, 5]])
+    expect(gameOfLife.getBoard()).toStrictEqual(
         [
             [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
         ]
-    );
-});
+    )
+})
 
-test('check row', () => {
-    let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
-    expect(main.checkRow(grid, [1, 3])).toBe(0);
-});
-
-
-test('check columns', () => {
-    let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
-    expect(main.checkColumns(grid, [2, 3])).toBe(2);
-});
-
-
-test('next tick', () => {
-    let grid = main.init(4, 8, [[3, 4], [3, 5], [4, 4]])
-    expect(main.nextTick(grid)).toStrictEqual(
+test("next tick", () => {
+    gameOfLife.createBoard(4, 8, [[2, 5], [3, 4], [3, 5]])
+    expect(gameOfLife.nextTick()).toStrictEqual(
         [
             [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0, 0],
         ]
-    );
-});
+    )
+})
